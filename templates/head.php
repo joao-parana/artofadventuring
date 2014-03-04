f17e8a8cfc9b0d42b9cfde4909055781c0328e42
Michael Tieso <vskylabv@gmail.com>
2014-03-04 11:51:21 -0600

    add favicons

--- templates/head.php
+++ templates/head.php
@@ -12,4 +12,18 @@
 	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
+
+	<link rel="apple-touch-icon" href="//artofadventuring.com/wp-content/fav/touchicon.png">
+	<link rel="icon" href="//artofadventuring.com/wp-content/fav/favicon.png"> 
+	<!--[if IE]><link rel="shortcut icon" href="//artofadventuring.com/wp-content/fav/favicon.ico"><![endif]--> 
+	<meta name="msapplication-TileColor" content="#2c201c"> 
+	<meta name="msapplication-TileImage" content="//artofadventuring.com/wp-content/fav/tileicon.png"> 
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="//artofadventuring.com/wp-content/fav/apple-touch-icon-144x144-precomposed.png"> 
+	<link rel="apple-touch-icon-precomposed" sizes="120x120" href="//artofadventuring.com/wp-content/fav/apple-touch-icon-120x120-precomposed.png"> 
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="//artofadventuring.com/wp-content/fav/apple-touch-icon-114x114-precomposed.png"> 
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="//artofadventuring.com/wp-content/fav/apple-touch-icon-72x72-precomposed.png"> 
+	<link rel="apple-touch-startup-image" media="(max-device-width: 480px) and not (-webkit-min-device-pixel-ratio: 2)" href="//artofadventuring.com/wp-content/fav/startup-iphone.png"> 
+	<link rel="apple-touch-startup-image" media="(max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2)" href="//artofadventuring.com/wp-content/fav/startup-iphone4.png"> 
+	<link rel="apple-touch-startup-image" media="(min-device-width: 768px) and (orientation: portrait)" href="//artofadventuring.com/wp-content/fav/startup-iPad-portrait.png"> 
+	<link rel="apple-touch-startup-image" media="(min-device-width: 768px) and (orientation: landscape)" href="//artofadventuring.com/wp-content/fav/startup-iPad-landscape.png">
 </head>
