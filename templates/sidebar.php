e1b8b312324ae40279db5840e443ff0980928e54
Michael Tieso <vskylabv@gmail.com>
2014-03-04 11:44:30 -0600

    facet

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,5 +1,7 @@
+<?php echo do_shortcode('[facetwp selections="true"]'); ?>
 <h3>Select a Country</h3>
 <?php echo do_shortcode('[facetwp facet="country"]'); ?>
 <h3>Category</h3>
 <?php echo do_shortcode('[facetwp facet="category"]'); ?>
+
 <?php dynamic_sidebar('sidebar-primary'); ?>
