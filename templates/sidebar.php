2cf687b20bcbbd97cbb215be2530c78fb784db09
Michael Tieso <vskylabv@gmail.com>
2014-03-04 11:04:03 -0600

    remove dates

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -2,6 +2,4 @@
 <?php echo do_shortcode('[facetwp facet="country"]'); ?>
 <h3>Category</h3>
 <?php echo do_shortcode('[facetwp facet="category"]'); ?>
-<h3>Date</h3>
-<?php echo do_shortcode('[facetwp facet="published_date"]'); ?>
 <?php dynamic_sidebar('sidebar-primary'); ?>
