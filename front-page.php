a04023c2d8dca774dc51b90448e993336b7d84cd
Michael Tieso <vskylabv@gmail.com>
2014-03-04 16:51:19 -0600

    Remove default pagination because facet is replacing it

--- front-page.php
+++ front-page.php
@@ -5,14 +5,6 @@
 		<div class="col-sm-8">
 			<?php echo do_shortcode('[facetwp template="front_page"]'); ?>
 			<?php echo do_shortcode('[facetwp pager="true"]'); ?>
-			<?php if ($wp_query->max_num_pages > 1) : ?>
-			  <nav class="post-nav">
-			    <ul class="pager">
-			      <li class="previous"><?php next_posts_link(__('&larr; Older posts', 'roots')); ?></li>
-			      <li class="next"><?php previous_posts_link(__('Newer posts &rarr;', 'roots')); ?></li>
-			    </ul>
-			  </nav>
-			<?php endif; ?>
 		</div>
 	  <aside class="sidebar col-sm-4" role="complementary">
       <?php include roots_sidebar_path(); ?>
