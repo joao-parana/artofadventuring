bc8a1393f100cf508bc7aefba0aa7c02837f936f
Michael Tieso <vskylabv@gmail.com>
2014-03-04 14:43:32 -0600

    Add text to select a category

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,7 +1,7 @@
 <?php echo do_shortcode('[facetwp selections="true"]'); ?>
 <h3>Select a Country</h3>
 <?php echo do_shortcode('[facetwp facet="country"]'); ?>
-<h3>Category</h3>
+<h3>Select a Category</h3>
 <?php echo do_shortcode('[facetwp facet="category"]'); ?>
 
 <?php dynamic_sidebar('sidebar-primary'); ?>
