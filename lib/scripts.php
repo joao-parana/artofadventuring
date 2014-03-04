16874560a8f27df4ac431bbe9aa9b1cade92b041
Michael Tieso <vskylabv@gmail.com>
2014-03-04 11:06:06 -0600

    increase font of sidebar for facetwp

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e6ebd5453a1426a88a684a2e85b6b474');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f508cda3c203a2bddd065874e0e55422');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
