fd2bb60071ad1d8f269c40c8c85dc3ce029c6535
Michael Tieso <vskylabv@gmail.com>
2014-03-05 09:13:54 -0600

    Change text in sidebar

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,5 +1,5 @@
 <?php echo do_shortcode('[facetwp selections="true"]'); ?>
-<h3>Select a Country</h3>
+<h3>Select a Region</h3>
 <?php echo do_shortcode('[facetwp facet="country"]'); ?>
 <h3>Select a Category</h3>
 <?php echo do_shortcode('[facetwp facet="category"]'); ?>
