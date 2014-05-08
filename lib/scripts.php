20ab8c28a52085c87acc13bb6b7baf140ba9d3b8
Michael Tieso <vskylabv@gmail.com>
2014-05-08 09:58:29 -0400

    Update

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0639bf179f0d83689b899e815b108b67');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b176a5fb06cb0d07a411bffe477f8e05');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
